21dabe6abafaf5cb821f5bea80fad6dd2b673fa3
Ademola <kolawoleoluwasegun567@gmail.com>
2022-07-27 15:13:02 +0100

    Adding Background

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart';
@@ -15,29 +17,50 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late ARKitController arkitController;
+  Timer? timer;
 
   @override
   void dispose() {
+    timer?.cancel();
     arkitController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    void onARKitViewCreated(ARKitController arkitController) {
-      this.arkitController = arkitController;
-      final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1),
-        position: Vector3(0, 0, -0.5),
-      );
-      this.arkitController.add(node);
-    }
-
     return MaterialApp(
         theme: ThemeData(),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(title: const Text('ARKit in Flutter')),
+            appBar: AppBar(
+                backgroundColor: Color(0xFF76B536),
+                title: const Text('Wimtorq ARView')),
             body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated)));
+  }
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+
+    final material = ARKitMaterial(
+      lightingModelName: ARKitLightingModel.lambert,
+      diffuse: ARKitMaterialProperty.image('assets/wimtorq-logo.jpg'),
+    );
+    final sphere = ARKitSphere(
+      materials: [material],
+      radius: 0.5,
+    );
+
+    final node = ARKitNode(
+      geometry: sphere,
+      position: Vector3(0, 0, -0.5),
+      eulerAngles: Vector3.zero(),
+    );
+    this.arkitController.add(node);
+
+    timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      final rotation = node.eulerAngles;
+      rotation.x += 0.01;
+      node.eulerAngles = rotation;
+    });
   }
 }
